92bb418794a9808b2609b70cc48c36579507a2b4
Josh Shone <josh.shone@gmail.com>
2020-03-10 22:48:07 +0100

    Rearrange and rename some code

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ if (location.protocol !== 'file:') {
   navigator.serviceWorker.register('service-worker.js', {scope: './'});
 }
 
+const piano = document.getElementById('piano');
+
 let recordedEvents = [];
 let currentSongTimeSeconds = 0;
 let recordingTickTimer = null;
@@ -11,134 +13,6 @@ let timestampOnLastRecordedEvent = null;
 
 let playbackTimer = null;
 const buttonsTouchedForPlayback = new Set();
-
-const piano = document.getElementById('piano');
-
-// Setup audio
-const audioContext = new (window.AudioContext || window.webkitAudioContext);
-for (const button of piano.getElementsByTagName('button')) {
-  button.envelope = audioContext.createGain();
-  button.envelope.gain.setValueAtTime(0, audioContext.currentTime);
-  button.envelope.connect(audioContext.destination);
-
-  button.osc = audioContext.createOscillator();
-  button.osc.frequency.setValueAtTime(parseFloat(button.dataset.freq), audioContext.currentTime);
-  button.osc.type = 'square';
-  button.osc.connect(button.envelope);
-  button.osc.start();
-}
-
-// So that multiple touches on the same button only result in one button activation and one note
-// played, the number of simultaneous touches for each button is counted.
-// Mouse, keyboard and touch events are all counted as touches.
-for (const button of piano.getElementsByTagName('button')) {
-  button.touches = 0;
-}
-function addTouchToPianoButton(button) {
-  button.touches++;
-  if (button.touches === 1) {
-    onPianoButton(button.dataset.freq, true);
-    button.classList.add('pressed');
-  }
-}
-function removeTouchFromPianoButton(button) {
-  button.touches--;
-  if (button.touches === 0) {
-    onPianoButton(button.dataset.freq, false);
-    button.classList.remove('pressed');
-  }
-}
-
-const touchMap = new Map();
-piano.ontouchstart = piano.ontouchmove = event => {
-  event.preventDefault();
-  for (const touch of event.changedTouches) {
-    const element = document.elementFromPoint(touch.clientX, touch.clientY);
-    if (element && element.tagName === 'BUTTON' && element.closest('#piano')) {
-      const previousButton = touchMap.get(touch.identifier);
-      if (previousButton !== element) { // Has this touch moved to a different button?
-        if (previousButton) {
-          removeTouchFromPianoButton(previousButton);
-        }
-        touchMap.set(touch.identifier, element);
-        addTouchToPianoButton(element);
-      }
-    }
-  }
-}
-piano.ontouchend = piano.ontouchcancel = event => {
-  for (const touch of event.changedTouches) {
-    const button = touchMap.get(touch.identifier);
-    if (button) {
-      removeTouchFromPianoButton(button);
-    }
-    touchMap.delete(touch.identifier);
-  }
-}
-
-piano.onmousedown = piano.onmouseover = event => {
-  if (event.buttons == 1 && event.target.tagName === 'BUTTON') {
-    addTouchToPianoButton(event.target);
-    event.target.onmouseup = () => {
-      removeTouchFromPianoButton(event.target);
-      event.target.onmouseup = null;
-    }
-  }
-}
-piano.onmouseup = piano.onmouseout = event => {
-  if (event.buttons == 1 && event.target.tagName === 'BUTTON') {
-    removeTouchFromPianoButton(event.target);
-  }
-}
-
-window.onkeydown = event => {
-  if (document.activeElement === document.getElementById('current-song-title')) {
-    return;
-  }
-  if (event.ctrlKey || event.shiftKey || event.altKey) {
-    return;
-  }
-  if (event.key === 'r') {
-    toggleRecording();
-  } else if (event.key === ' ') {
-    event.preventDefault();
-    togglePlayback();
-  } else {
-    const button = piano.querySelector(`[data-key="${event.key}"]`);
-    if (button && !button.isKeyDown) {
-      document.body.classList.add('keyboard-enabled');
-      event.preventDefault();
-      button.isKeyDown = true; // To debounce repeated keydown events, from keys held down
-      addTouchToPianoButton(button);
-    }
-  }
-}
-window.onkeyup = event => {
-  const button = piano.querySelector(`[data-key="${event.key}"]`);
-  if (button && button.isKeyDown) {
-    button.isKeyDown = false;
-    removeTouchFromPianoButton(button);
-  }
-}
-
-function onPianoButton(freq, state) {
-  if (audioContext.state === 'suspended') {
-    // The AudioContext will initially be in a suspended state, and is only allowed
-    // to be resumed here, in response to a user gesture.
-    // See https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API/Best_practices#Autoplay_policy
-    audioContext.resume();
-  }
-  const envelope = piano.querySelector(`[data-freq="${freq}"]`).envelope;
-  envelope.gain.setValueAtTime(state ? 0.02 : 0, audioContext.currentTime);
-  if (document.body.dataset.mode === 'recording') {
-    const now = performance.now();
-    const timeSinceLastEvent = now - timestampOnLastRecordedEvent;
-    recordedEvents.push({freq, state, delay: timeSinceLastEvent});
-    timestampOnLastRecordedEvent = now;
-    document.getElementById('play-button').disabled = false;
-    document.getElementById('save-button').disabled = false;
-  }
-}
 
 function toggleRecording() {
   if (document.body.dataset.mode !== 'recording') {
@@ -256,4 +130,132 @@ for (const song of localStorageSongs) {
 
 function secondsToDisplayString(seconds) {
   return String(Math.floor(seconds / 60)).padStart(2, '0') + ':' + String(seconds % 60).padStart(2, '0');
+}
+
+// Setup audio
+const audioContext = new (window.AudioContext || window.webkitAudioContext);
+for (const button of piano.getElementsByTagName('button')) {
+  button.osc = audioContext.createOscillator();
+  button.osc.frequency.setValueAtTime(parseFloat(button.dataset.freq), audioContext.currentTime);
+  button.osc.type = 'square';
+  button.osc.start();
+
+  button.envelope = audioContext.createGain();
+  button.envelope.gain.setValueAtTime(0, audioContext.currentTime);
+  button.osc.connect(button.envelope);
+  button.envelope.connect(audioContext.destination);
+}
+
+function onPianoButtonStateChanged(freq, state) {
+  if (audioContext.state === 'suspended') {
+    // The AudioContext will initially be in a suspended state, and is only allowed
+    // to be resumed here, in response to a user gesture.
+    // See https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API/Best_practices#Autoplay_policy
+    audioContext.resume();
+  }
+
+  const envelope = piano.querySelector(`[data-freq="${freq}"]`).envelope;
+  envelope.gain.setValueAtTime((state === 'pressed') ? 0.02 : 0, audioContext.currentTime);
+
+  if (document.body.dataset.mode === 'recording') {
+    const now = performance.now();
+    const timeSinceLastEvent = now - timestampOnLastRecordedEvent;
+    recordedEvents.push({freq, state: state === 'pressed', delay: timeSinceLastEvent});
+    timestampOnLastRecordedEvent = now;
+    document.getElementById('play-button').disabled = false;
+    document.getElementById('save-button').disabled = false;
+  }
+}
+
+// So that multiple touches on the same button only result in one button 'press' and one note
+// played, the number of simultaneous touches for each button is counted.
+// Mouse, keyboard and touch events are all counted as touches.
+for (const button of piano.getElementsByTagName('button')) {
+  button.touches = 0;
+}
+function addTouchToPianoButton(button) {
+  button.touches++;
+  if (button.touches === 1) {
+    button.classList.add('pressed');
+    onPianoButtonStateChanged(button.dataset.freq, 'pressed');
+  }
+}
+function removeTouchFromPianoButton(button) {
+  button.touches--;
+  if (button.touches === 0) {
+    button.classList.remove('pressed');
+    onPianoButtonStateChanged(button.dataset.freq, 'unpressed');
+  }
+}
+
+const touchMap = new Map();
+piano.ontouchstart = piano.ontouchmove = event => {
+  event.preventDefault();
+  for (const touch of event.changedTouches) {
+    const element = document.elementFromPoint(touch.clientX, touch.clientY);
+    if (element && element.closest('#piano') && element.tagName === 'BUTTON') {
+      const previousButton = touchMap.get(touch.identifier);
+      if (previousButton !== element) { // Has this touch moved to a different button?
+        if (previousButton) {
+          removeTouchFromPianoButton(previousButton);
+        }
+        touchMap.set(touch.identifier, element);
+        addTouchToPianoButton(element);
+      }
+    }
+  }
+}
+piano.ontouchend = piano.ontouchcancel = event => {
+  for (const touch of event.changedTouches) {
+    const button = touchMap.get(touch.identifier);
+    if (button) {
+      removeTouchFromPianoButton(button);
+    }
+    touchMap.delete(touch.identifier);
+  }
+}
+
+piano.onmousedown = piano.onmouseover = event => {
+  if (event.buttons == 1 && event.target.tagName === 'BUTTON') {
+    addTouchToPianoButton(event.target);
+    event.target.onmouseup = () => {
+      removeTouchFromPianoButton(event.target);
+      event.target.onmouseup = null;
+    }
+  }
+}
+piano.onmouseup = piano.onmouseout = event => {
+  if (event.buttons == 1 && event.target.tagName === 'BUTTON') {
+    removeTouchFromPianoButton(event.target);
+  }
+}
+
+window.onkeydown = event => {
+  if (document.activeElement === document.getElementById('current-song-title')) {
+    return;
+  }
+  if (event.ctrlKey || event.shiftKey || event.altKey) {
+    return;
+  }
+  if (event.key === 'r') {
+    toggleRecording();
+  } else if (event.key === ' ') {
+    event.preventDefault();
+    togglePlayback();
+  } else {
+    const button = piano.querySelector(`[data-key="${event.key}"]`);
+    if (button && !button.isKeyDown) {
+      document.body.classList.add('keyboard-enabled');
+      event.preventDefault();
+      button.isKeyDown = true; // To debounce repeated keydown events, from keys held down
+      addTouchToPianoButton(button);
+    }
+  }
+}
+window.onkeyup = event => {
+  const button = piano.querySelector(`[data-key="${event.key}"]`);
+  if (button && button.isKeyDown) {
+    button.isKeyDown = false;
+    removeTouchFromPianoButton(button);
+  }
 }
